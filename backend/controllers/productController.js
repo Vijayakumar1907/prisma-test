c1a541942e7fddd5a9dcd7e47c16a10990894364
Mukunthan M <63898896+mukunthan7@users.noreply.github.com>
2023-02-23 13:25:40 +0530

    Revert "added More Controllers And Prisma Schema"

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,43 +1,11 @@
 import {PrismaClient} from "@prisma/client"
-import { Router } from "express";
 
 const prisma = new PrismaClient();
-const router = Router();
 
-const getProducts = async(req,res) => {
+export const getProducts = async(req,res) => {
     try{
-        const response = await prisma.product.findMany({
-            include: {
-                supplierName: true
-            }
-        });
-        res.status(200).json(response);
+        const response = await prisma.
     }catch(error){
-        console.log(error);
+
     }
 }
-
-const postProduct = async(req,res) => {
-    try{
-        const { productName, productDescription, supplierName } = req.body;
-        const response = await prisma.product.create({
-            data: {
-                productName,
-                productDescription,
-                supplierName : {
-                    connect: {
-                        supplierName
-                    }
-                }
-            }
-        });
-        res.status(200).json(response);
-    }catch(error){
-        console.log(error);
-    }
-}
-
-router.get("/", getProducts);
-router.post("/", postProduct);
-
-export default router;
